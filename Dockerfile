31b6a02ad18389e88e944212dbdfea9e0379dc09
yutoogi <jacoyutorius@gmail.com>
2021-09-18 07:52:12 +0900

    crawl akaden info page

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM ruby:3.0.2-alpine
 
+RUN apk update
+RUN apk add build-base libxml2-dev libxslt-dev
+
 WORKDIR /app
 COPY Gemfile /app/
 RUN bundle
--- app.rb
+++ app.rb
@@ -4,4 +4,4 @@ $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
 
 require 'akaden'
 
-pp ::Akaden::Info.crawl
+pp ::Akaden::Info.run(station: 'kamijima')
